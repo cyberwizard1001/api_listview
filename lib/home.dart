fc629ef508579edaa868ee332c6ea39c2e637da3
Nirmal K <nirmalkarthikeyan1503@gmail.com>
2022-02-11 19:52:03 +0530

    Added basic functionality, google_fonts to pubspec.yaml

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:api_listview/users_listview.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -13,10 +14,13 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('Users'),
+        title: Text('Users',style: GoogleFonts.poppins(color: Colors.black),),
+        elevation: 0,
+        backgroundColor: Colors.white,
       ),
-      body: UsersListView(),
+      body: const UsersListView(),
     );
   }
 
--- lib/users_listview.dart
+++ lib/users_listview.dart
@@ -24,6 +24,18 @@ class User {
     );
   }
 
+}
+
+class UsersListView extends StatefulWidget {
+  const UsersListView({Key? key}) : super(key: key);
+
+  @override
+  _UsersListViewState createState() => _UsersListViewState();
+}
+
+class _UsersListViewState extends State<UsersListView> {
+
+
   Future<List<User>> _fetchUsers() async {
 
     const userAPIURL = 'https://jsonplaceholder.typicode.com/users';
@@ -36,19 +48,42 @@ class User {
       throw Exception('Unable to establish a connection with the API. Please check your network (or, the API might be down)');
     }
   }
-}
 
-class UsersListView extends StatefulWidget {
-  const UsersListView({Key? key}) : super(key: key);
+  ListView _userListView(data) {
+    return ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (context, index) {
+          return _tile(data[index].name, data[index].username, data[index].email, Icons.work);
+        });
+  }
 
-  @override
-  _UsersListViewState createState() => _UsersListViewState();
-}
+  ListTile _tile(String name, String username, String email,  IconData icon) => ListTile(
+    title: Text(name,
+        style: const TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 20,
+        )),
+    subtitle: Text(username),
+    leading: Icon(
+      icon,
+      color: Colors.blue[500],
+    ),
+  );
 
-class _UsersListViewState extends State<UsersListView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return FutureBuilder<List<User>>(
+      future: _fetchUsers(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          List<User>? data = snapshot.data;
+          return _userListView(data);
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+        return const CircularProgressIndicator();
+      },
+    );;
   }
 }
 
